92a708213c7adf52b3f501085a0ff0fe4e33b8fa
ikhsan6012 <ikhsan6012.com>
2019-07-09 07:57:04 +0700

    Beberapa Perbaikan

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,4 @@
 import React, { Fragment, useState } from 'react'
-
 import { login, logout } from '../functions/auth'
 
 const Nav = () => {
@@ -16,36 +15,39 @@ const Nav = () => {
         form.forEach(input => input.value = '')
       }
     }, 100)
-  }
+	}
 
 	return(
 		<nav className="main-header navbar navbar-expand bg-white navbar-light border-bottom">
 			<ul className="navbar-nav ml-auto">
-				<li className="nav-item">
-					{ localStorage.getItem('token') ? 
-						<Fragment>
-							<span className="mr-2">{ JSON.parse(localStorage.getItem('user')).nama }</span>
+				{ localStorage.getItem('token') ? 
+					<Fragment>
+						<li className="nav-item align-middle mr-2 dropdown" title="Ganti Password">
+							<div className="nav-link dropdown-toggle" style={{ cursor: 'pointer' }}>{ JSON.parse(localStorage.getItem('user')).nama }</div>
+						</li>
+						<li className="nav-item">
 							<button className="btn btn-danger" onClick={ logout }>Logout</button>
-						</Fragment> :
-						<form className="form-inline" id="formLogin">
-							<li className="nav-item mr-2">
-								<div className="input-group">
-									<input type="text" className="form-control" placeholder="Username" hidden={ isHiddenLogin }/>
-								</div>
-							</li>
-							<li className="nav-item mr-2">
-								<div className="input-group">
-									<input type="password" className="form-control" placeholder="Password" hidden={ isHiddenLogin }/>
-								</div>
-							</li>
-							<li className="nav-item mr-2">
-								<button type="reset" className="btn btn-secondary" hidden={ isHiddenLogin } onClick={ showLogin }>Batal</button>
-							</li>
-							<li className="nav-item">
-								<button type="submit" className="btn btn-primary" onClick={ isHiddenLogin ? showLogin : login }>Login</button>
-							</li>
-						</form> }
-				</li>
+						</li>
+					</Fragment> :
+					<form className="form-inline" id="formLogin">
+						<li className="nav-item mr-2">
+							<div className="input-group">
+								<input type="text" className="form-control" placeholder="Username" hidden={ isHiddenLogin }/>
+							</div>
+						</li>
+						<li className="nav-item mr-2">
+							<div className="input-group">
+								<input type="password" className="form-control" placeholder="Password" hidden={ isHiddenLogin }/>
+							</div>
+						</li>
+						<li className="nav-item mr-2">
+							<button type="reset" className="btn btn-secondary" hidden={ isHiddenLogin } onClick={ showLogin }>Batal</button>
+						</li>
+						<li className="nav-item">
+							<button type="submit" className="btn btn-primary" onClick={ isHiddenLogin ? showLogin : login }>Login</button>
+						</li>
+					</form>
+				}
 			</ul>
 		</nav>
 	)
